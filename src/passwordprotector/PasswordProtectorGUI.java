80e3b34c5a850ff70c1fd982160e2bf4119678dd
m-yuhas <m-yuhas@qq.com>
2017-09-28 22:56:42 -0700

    Continuing Gui Work

--- src/passwordprotector/PasswordProtectorGUI.java
+++ src/passwordprotector/PasswordProtectorGUI.java
@@ -2,6 +2,7 @@ package passwordprotector;
 
 import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class PasswordProtectorGUI {
@@ -11,6 +12,16 @@ public class PasswordProtectorGUI {
 		this.mainFrame = new JFrame("Password Protector Beta");
 		this.mainFrame.setSize(400,400);
 		this.mainFrame.setLayout(new GridLayout(3,1));
+		
+		JButton openFileButton = new JButton("Open");
+		JButton newButton = new JButton("New");
+		JButton generateRandomStringButton = new JButton("Generate Random String");
+		
+		mainFrame.add(openFileButton);
+		mainFrame.add(newButton);
+		mainFrame.add(generateRandomStringButton);
+		
+		
 		
 		this.mainFrame.setVisible(true);
 	}
