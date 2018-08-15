b2afed3a57999ae263e49f3e0782c1e0dfe02f60
m-yuhas <m-yuhas@qq.com>
2018-08-15 01:10:08 -0700

    continuing work on password_protector.go

--- cli/password_protector.go
+++ cli/password_protector.go
@@ -4,11 +4,13 @@
 package main
 
 import (
+  "bufio"
   "flag"
   "fmt"
   "golang.org/x/crypto/ssh/terminal"
   "os"
   "password_protector/password_protector"
+  "strings"
   "syscall"
 )
 
@@ -54,5 +56,68 @@ func usage() {
 }
 
 func mainMenu(records []password_protector.AccountRecord, newFile bool, password []byte) {
+  promptToSave := false
+  modified := false
+  for {
+    fmt.Println("Options:\n1. LIST accounts\n2. ADD account\n3. DELETE account\n4. MODIFY account\n5. CHANGE file password\n6. SAVE\n7. QUIT")
+    reader := bufio.NewReader(os.Stdin)
+    selection, err := reader.ReadString('\n')
+    if err != nil {
+      fmt.Println("An error occurred while reading the input.")
+      continue
+    }
+    selectionArray := strings.Fields(selection)
+    if len(selectionArray) < 1 {
+      fmt.Println("Invalid selection")
+      continue
+    }
+    selection = strings.ToLower(selectionArray[0])
+    switch {
+    case selection == "list" || selection == "1":
+      list(records)
+    case selection == "add" || selection == "2":
+      records, modified = add(records)
+    case selection == "delete" || selection == "3":
+      records, modified = delete(records, selectionArray)
+    case selection == "modify" || selection == "4":
+      records, modified = modify(records, selectionArray)
+    case selection == "change" || selection == "5":
+      password, modified = change(password)
+    case selection == "save" || selection == "6":
+      modified = save(records, password)
+    case selection == "quit" || selection == "7":
+      quit(records, promptToSave)
+    default:
+      fmt.Println("Sorry, I didn't understand.")
+    }
+    promptToSave = promptToSave || modified
+  }
+}
 
+func list(records []password_protector.AccountRecord) {
+  return
+}
+
+func add(records []password_protector.AccountRecord) ([]password_protector.AccountRecord, bool) {
+  return []password_protector.AccountRecord{}, false
+}
+
+func delete(records []password_protector.AccountRecord, selectionArray []string) ([]password_protector.AccountRecord, bool) {
+  return []password_protector.AccountRecord{}, false
+}
+
+func modify(records []password_protector.AccountRecord, selectionArray []string) ([]password_protector.AccountRecord, bool) {
+  return []password_protector.AccountRecord{}, false
+}
+
+func change(password []byte) ([]byte, bool) {
+  return []byte{}, false
+}
+
+func save(records []password_protector.AccountRecord, password []byte) bool {
+  return false
+}
+
+func quit(records []password_protector.AccountRecord, modified bool) {
+  os.Exit(0)
 }
