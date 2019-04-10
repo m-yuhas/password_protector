92e94fba3da9be982ea1d074954ebedd407d6ee9
m-yuhas <m-yuhas@qq.com>
2019-04-09 22:57:29 -0700

    Starting GUI work

---
+++ gui/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/therecipe/qt/widgets"
+	"os"
+)
+
+func main() {
+	app := widgets.NewQApplication(len(os.Args), os.Args)
+	window := widgets.NewQMainWindow(nil, 0)
+	window.SetWindowTitle("Password Protector")
+	window.SetMinimumSize2(640, 480)
+    menu := widgets.NewQMenuBar(window)
+    fileMenu := widgets.NewQMenu2("File", menu)
+    fileMenu.AddAction("New")
+    fileMenu.AddAction("Open")
+    fileMenu.AddAction("Save")
+    fileMenu.AddAction("Change File Password")
+    fileMenu.AddAction("Quit")
+    passwordMenu := widgets.NewQMenu2("Password", menu)
+    passwordMenu.AddAction("Generate Password")
+    //helpMenu := widgets.NewQMenu2("Help", menu)
+    //helpMenu.AddAction("Documentation")
+    //helpMenu.AddAction("About")
+    menu.AddMenu(fileMenu)
+    menu.AddMenu(passwordMenu)
+    //menu.AddMenu(helpMenu)
+    window.SetMenuBar(menu)
+
+
+	layout := widgets.NewQVBoxLayout()
+	mainWidget := widgets.NewQWidget(nil, 0)
+	mainWidget.SetLayout(layout)
+
+	// Set main widget as the central widget of the window
+	window.SetCentralWidget(mainWidget)
+	window.Show()
+	app.Exec()
+}
