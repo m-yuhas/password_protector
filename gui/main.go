0afd1200e856c6cb1779b38e9dd4e993e5642219
m-yuhas <m-yuhas@qq.com>
2019-04-15 23:43:57 -0700

    Rearranged Menu Bar

--- gui/main.go
+++ gui/main.go
@@ -1,40 +1,97 @@
 package main
 
 import (
+    "github.com/therecipe/qt/core"
     "github.com/therecipe/qt/gui"
 	"github.com/therecipe/qt/widgets"
 	"os"
 )
 
+type PasswordProtector struct {
+    widgets.QMainWindow
+    actionNew *widgets.QAction
+    actionOpen *widgets.QAction
+    actionSave *widgets.QAction
+    actionChangePassword *widgets.QAction
+    actionQuit *widgets.QAction
+    actionEncryptFile *widgets.QAction
+    actionDecryptFile *widgets.QAction
+    actionGeneratePassword *widgets.QAction
+    actionHelp *widgets.QAction
+    actionAbout *widgets.QAction
+    fileName string
+    records map[string][]byte{}
+}
+
+
 func main() {
 	app := widgets.NewQApplication(len(os.Args), os.Args)
-	window := widgets.NewQMainWindow(nil, 0)
-	window.SetWindowTitle("Password Protector")
-	window.SetMinimumSize2(640, 480)
-    menu := widgets.NewQMenuBar(window)
-    fileMenu := widgets.NewQMenu2("File", menu)
-    fileMenu.AddAction3("New", nil, "test", gui.NewQKeySequence5(gui.QKeySequence__New))
-    fileMenu.AddAction("Open")
-    fileMenu.AddAction("Save")
-    fileMenu.AddAction("Change File Password")
-    fileMenu.AddAction("Quit")
-    passwordMenu := widgets.NewQMenu2("Password", menu)
-    passwordMenu.AddAction("Generate Password")
-    //helpMenu := widgets.NewQMenu2("Help", menu)
-    //helpMenu.AddAction("Documentation")
-    //helpMenu.AddAction("About")
-    menu.AddMenu(fileMenu)
-    menu.AddMenu(passwordMenu)
-    //menu.AddMenu(helpMenu)
-    window.SetMenuBar(menu)
+    core.QCoreApplication_SetApplicationName("Password Protector")
+    core.QCoreApplication_SetApplicationVersion("0.0.1")
+    mainWindow := initPasswordProtector()
+    /* TODO: Work on parsing args for drag and drop
+    if !mainWindow.open(args[0]) {
+        mainWindow.newFile()
+    }*/
+    mainWindow.Show()
+    app.Exec()
+}
 
+func initPasswordProtector() *PasswordProtector {
+    var this = NewPasswordProtector(nil, 0)
+    this.SetWindowTitle(core.QCoreApplication_ApplicationName())
+    this.SetMinimumSize2(640, 480)
+    this.setupFileActions()
+    this.setupSecurityActions()
+    this.setupHelpActions()
+    return this
+}
 
-	layout := widgets.NewQVBoxLayout()
-	mainWidget := widgets.NewQWidget(nil, 0)
-	mainWidget.SetLayout(layout)
+func (p *PasswordProtector) setupFileActions() {
+    menu := p.MenuBar().AddMenu2("File")
+    p.actionNew = menu.AddAction("New")
+    p.actionNew.ConnectTriggered(func(checked bool) { p.foo()})
+    p.actionNew.SetShortcuts2(gui.QKeySequence__New)
+    p.actionOpen = menu.AddAction("Open")
+    p.actionOpen.ConnectTriggered(func(checked bool) { p.foo()})
+    p.actionOpen.SetShortcuts2(gui.QKeySequence__Open)
+    p.actionSave = menu.AddAction("Save")
+    p.actionSave.ConnectTriggered(func(checked bool) { p.foo()})
+    p.actionSave.SetShortcuts2(gui.QKeySequence__Save)
+    p.actionChangePassword = menu.AddAction("Change File Password")
+    p.actionChangePassword.ConnectTriggered(func(checked bool) { p.foo()})
+    p.actionQuit = menu.AddAction("Quit")
+    p.actionQuit.ConnectTriggered(func(checked bool) { p.foo() })
+    p.actionQuit.SetShortcuts2(gui.QKeySequence__Quit)
+}
 
-	// Set main widget as the central widget of the window
-	window.SetCentralWidget(mainWidget)
-	window.Show()
-	app.Exec()
+func (p *PasswordProtector) setupSecurityActions() {
+    menu := p.MenuBar().AddMenu2("Security")
+    p.actionEncryptFile = menu.AddAction("Encrypt File")
+    p.actionEncryptFile.ConnectTriggered(func(checked bool) {p.foo()})
+    p.actionDecryptFile = menu.AddAction("Decrypt File")
+    p.actionDecryptFile.ConnectTriggered(func(checked bool) {p.foo()})
+    p.actionGeneratePassword = menu.AddAction("Generate Password")
+    p.actionGeneratePassword.ConnectTriggered(func(checked bool) {p.foo()})
+}
+
+func (p *PasswordProtector) setupHelpActions() {
+    menu := p.MenuBar().AddMenu2("Help")
+    p.actionHelp = menu.AddAction("Password Protector Help")
+    p.actionHelp.ConnectTriggered(func(checked bool) {p.foo()})
+    p.actionAbout = menu.AddAction("About")
+    p.actionAbout.ConnectTriggered(func(checked bool) {p.about()})
+}
+
+func (p *PasswordProtector) foo() {
+    widgets.QMessageBox_About(p, "About", "This is a test")
+}
+
+func (p *PasswordProtector) about() {
+    widgets.QMessageBox_About(
+        p,
+        "About Password Protector",
+        "This application encrypts passwords for management. " +
+        "<a href='https://github.com/m-yuhas/password_protector'>click</a>",
+    )
 }
